45b423b2cd312e2bb3137b8ff66ddf43b8be4898
lewis2000mason <lewis2000mason@gmail.com>
2026-03-09 22:00:26 +0000

    Update shop-control.js

--- shop-control.js
+++ shop-control.js
@@ -1,1 +1,19 @@
+// SHOP CONTROL
+// Change this to true or false depending on whether the shop is open
 
+const shopOpen = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const shopSection = document.getElementById("shop-items");
+    const comingSoon = document.getElementById("shop-coming-soon");
+
+    if (shopOpen) {
+        shopSection.style.display = "grid";
+        comingSoon.style.display = "none";
+    } else {
+        shopSection.style.display = "none";
+        comingSoon.style.display = "flex";
+    }
+
+});
